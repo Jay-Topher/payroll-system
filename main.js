442caa143ef6d9e7da3c8e3c56eef5caadf057ba
Jones Ogolo <jay.topher11@gmail.com>
2019-05-30 21:31:47 +0100

    'Salary added'

--- main.js
+++ main.js
@@ -8,20 +8,24 @@ $(document).ready(function() {
     $.getJSON('http://localhost:3000/employees', function(data){
         let employeeData = "";
         $.each(data, function(key, value){
-            console.log(data)
+            // console.log(data)
             employeeData += '<tr>';
             employeeData += '<td>' + value.id + '</td>';
             employeeData += '<td>' + value.name + '</td>';
             employeeData += '<td>' + value.qualification + '</td>';
             employeeData += '<td>' + value.position + '</td>';
             employeeData += '<td>' + value.level + '</td>';
+            employeeData += '<td>' + value.salary + '</td>';
             employeeData += '<td>' + value.paymentStatus + '</td>';
-            employeeData += '<td><button class="deleteEmp" data-id="';
-            employeeData +=  value.id + '" type="button">X</td></tr>';
+            employeeData += '<td><button class="deleteEmp material-icons" data-id="';
+            employeeData += value.id + '" type="button">delete</button><a href="edit employee.html" id="editButton" class="material-icons" data-id="';
+            employeeData += value.id + '">edit</a></td></tr>'; // onclick="window.location.href=\'edit employee.html\'
         })
         $('#employTable').append(employeeData);
     })
 
+
+//  get form data and populate table
     $('#addButton').on('click', function(){
         let fullName = $('#fullname').val();
         let position = $('#position').val();
@@ -29,11 +33,41 @@ $(document).ready(function() {
         let level = $('#level').val();
         let paymentStatus = false;
 
+        function getSalary(level) {
+            switch (level) {
+                case 1:
+                    return 20000;
+                    break;
+                case 2:
+                    return 40000;
+                    break;
+                case 3:
+                    return 60000;
+                    break;
+                
+                case 4:
+                    return 80000;
+                    break;
+
+                case 5:
+                    return 100000;
+                    break;
+
+                case 6:
+                    return 120000;
+                    break;
+                default:
+                    return 150000;
+                    break;
+            }
+        }
+
         let employee = {
             name: fullName,
             position: position,
             qualification: qualification,
             level: level,
+            salary: getSalary(this.level),
             paymentStatus: paymentStatus
         };
 
@@ -60,5 +94,71 @@ $(document).ready(function() {
             }
         });
     })
+
+    // $('#editButton').on('click')
+    // $tableEmploy.delegate('#editButton', 'click', function(){
+    //     let id = $(this).attr('data-id');
+    //     let $td = $(this).closest('td');
+    //     console.log(id);
+    //     $('#fullname1').val(id.);
+        // $td.find('input.fullname').val()
+    // })
+
+    $('#addButton1').on('click', function(){
+        // let fullName = $('#fullname1').val();
+        // let position = $('#position1').val();
+        // let qualification = $('#qualification1').val();
+        // let level = $('#level1').val();
+        // let paymentStatus = false;
+
+        // let employee = {
+        //     name: fullName,
+        //     position: position,
+        //     qualification: qualification,
+        //     level: level,
+        //     paymentStatus: paymentStatus
+        // };
+
+
+        // $.ajax({
+        //     type: 'POST',
+        //     url: url + id,
+        //     data: employee,
+        //     success: function(emp){
+            
+        //     }
+        // })
+
+    })
+
+    // $('#addButton').on('click', function(){
+    //     let fullName = $('#fullname').val();
+    //     let position = $('#position').val();
+    //     let qualification = $('#qualification').val();
+    //     let level = $('#level').val();
+    //     let paymentStatus = false;
+
+        
+
+    //     let employee = {
+    //         name: fullName,
+    //         position: position,
+    //         qualification: qualification,
+    //         level: level,
+    //         salary: getSalary(level),
+    //         paymentStatus: paymentStatus
+    //     };
+
+
+    //     $.ajax({
+    //         type: 'POST',
+    //         url: url,
+    //         data: employee,
+    //         success: function(emp){
+            
+    //         }
+    //     })
+
+    // })
 
 });
